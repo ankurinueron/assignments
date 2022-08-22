fc5ccd883bcf9b284166c1824e1b9c3ea93f6fc4
ankurinueron <111796032+ankurinueron@users.noreply.github.com>
2022-08-23 01:01:59 +0530

    Add files via upload

---
+++ que10.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() 
+{
+    int hour,min;
+    printf("Enter the time in HH:MM format\n");
+    scanf("%d %d",&hour,&min);
+    printf("%d Hour and %d Minutes",hour,min);
+    return 0;
+
+}
---
+++ que8.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() 
+{
+    printf("\"Hello, Ankur Pal\"");
+    return 0;
+
+}
